4e4d24690d0883e8725ebe9e44241d9b6c7abefd
Augusto Pagnossim Frigo <frigo.augusto@gmail.com>
2022-11-12 19:58:43 -0300

    configured port and cross-origin request

--- src/main/java/com/example/hamburguerhelperback/controllers/HelloWorldController.java
+++ src/main/java/com/example/hamburguerhelperback/controllers/HelloWorldController.java
@@ -3,6 +3,7 @@ package com.example.hamburguerhelperback.controllers;
 import com.example.hamburguerhelperback.domain.entities.TesteOrder;
 import com.example.hamburguerhelperback.domain.repo.OrderRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,6 +12,7 @@ import java.security.InvalidParameterException;
 import java.util.List;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:8080")
 public class HelloWorldController{
     @Autowired
     private OrderRepo orderRepo;
