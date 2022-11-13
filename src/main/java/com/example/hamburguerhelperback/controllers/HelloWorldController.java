33e09e82444a47a2422389d5059291033a0b3bb2
Augusto Pagnossim Frigo <frigo.augusto@gmail.com>
2022-11-12 22:34:42 -0300

    added example of request body

--- src/main/java/com/example/hamburguerhelperback/controllers/HelloWorldController.java
+++ src/main/java/com/example/hamburguerhelperback/controllers/HelloWorldController.java
@@ -2,11 +2,9 @@ package com.example.hamburguerhelperback.controllers;
 
 import com.example.hamburguerhelperback.domain.entities.TesteOrder;
 import com.example.hamburguerhelperback.domain.repo.OrderRepo;
+import com.example.hamburguerhelperback.dto.ExampleDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.security.InvalidParameterException;
 import java.util.List;
@@ -34,11 +32,18 @@ public class HelloWorldController{
                 .orElseThrow(InvalidParameterException::new);
     }
 
-    @GetMapping("/insert-order")
+    @PostMapping("/insert-order")
     public void insertOrder(@RequestParam String name){
         TesteOrder order = new TesteOrder();
         order.setName(name);
         order.setDescription("batata");
         orderRepo.save(order);
+        System.out.println(name);
+    }
+
+    @PostMapping("/print-example-dto")
+    public void printExampleDto(@RequestBody ExampleDto exampleDto){
+        System.out.println(exampleDto.toString());
+        System.out.println(exampleDto.getName());
     }
 }
---
+++ src/main/java/com/example/hamburguerhelperback/dto/ExampleDto.java
@@ -0,0 +1,16 @@
+package com.example.hamburguerhelperback.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@AllArgsConstructor
+@Getter
+@Setter
+@ToString
+public class ExampleDto {
+    private String name;
+    private Integer age;
+    private String cellphone;
+}
